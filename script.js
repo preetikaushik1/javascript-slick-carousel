edc38e16b4f43c869d74d8407ab80c0c46e93cf7
preeti1098 <124133826+preeti1098@users.noreply.github.com>
2023-04-28 17:06:09 +0530

    new-work
    new-work

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slick-carousel1').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
+
+
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 5,
+        slidesToScroll: 1
+    });
+});
+
